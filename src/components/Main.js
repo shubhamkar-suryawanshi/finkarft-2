5f2a6fad4d62726e0c19da0407c7182f8dd8fea4
shubhamkar-suryawanshi <shubhamkar.kop@gmail.com>
2023-02-05 13:48:45 +0530

    adding different paths acc to dropdown value

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Main() {
+  const [selectValue, setSelectValue] = useState('todos');
+  const [path, setPath] = useState('/grid');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(selectValue);
+    if (selectValue === 'todos') setPath('/grid');
+    else setPath('/comments');
+  }
+
   return (
     <div className="App">
       <h1>Welcome to my Simple App</h1>
       <h2>What you wanted to see?</h2>
       <h3>My posts or My comments?</h3>
-      <form>
-        <label>Choose an option: </label>
-        <select id="selector">
-          <option value="todos">Todos</option>
-          <option value="comments">Comments</option>
-        </select>
-        <button>
-          <Link to={'/grid'}>Submit</Link>
+      <form onSubmit={handleSubmit} className="form">
+        <label>
+          Choose an option:
+          <select
+            value={selectValue}
+            onChange={(e) => {
+              setSelectValue(e.target.value);
+            }}
+          >
+            <option value="todos">Todos</option>
+            <option value="comments">Comments</option>
+          </select>
+        </label>
+        <button type="submit">
+          <Link to={path}>Submit</Link>
         </button>
       </form>
     </div>
